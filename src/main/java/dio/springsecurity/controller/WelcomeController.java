c3faa40b51f31f5c98500f616b003fffd6a6755a
Vguilhen <vinicius.guilhen2013@gmail.com>
2023-04-10 18:51:44 -0300

    basic Auth

--- src/main/java/dio/springsecurity/controller/WelcomeController.java
+++ src/main/java/dio/springsecurity/controller/WelcomeController.java
@@ -1,4 +1,4 @@
-package dio.springsecurity;
+package dio.springsecurity.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
